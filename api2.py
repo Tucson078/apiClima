1d136dd78ec1296e490cfac1d28d799836c27f07
kennybell078 <agustin.ruiz@sagradoalcal.edu.ar>
2023-07-14 15:05:53 -0300

    Api2AgregadosDatosHoy

--- api2.py
+++ api2.py
@@ -3,7 +3,7 @@ import requests
 class Clima():
     city = ""
     api_key = "03b9403c9b7130a2cc695d264a22c2c1"
-    temp = 0
+    temperaturaHoy = 0
     description = 0
     humidity = 0
 
@@ -11,12 +11,16 @@ class Clima():
     def obtener_clima(self, city):
         self.city = city
         self.url = f"https://api.openweathermap.org/data/2.5/forecast?q={self.city}&appid={self.api_key}&units=metric&cnt=7"
+        self.urlVieja = f"https://api.openweathermap.org/data/2.5/weather?q={self.city}&appid={self.api_key}&units=metric"
         res = requests.get(self.url)
+        resVieja = requests.get(self.urlVieja)
         data = res.json()
+        dataVieja = resVieja.json()
 
-        self.temp = data["main"]["temp"]
-        
-        self.description = data["weather"][0]["description"]
-        self.humidity = data["main"]["humidity"]
+        self.tempHoy = dataVieja["main"]["temp"]
+        self.sensacionTermica = dataVieja["main"]["feels_like"]
+        self.tempMaxHoy = dataVieja["main"]["temp_max"]
+        self.tempMinHoy = dataVieja["main"]["temp_min"]
 
-
+clima = Clima()
+clima.obtener_clima("Buenos Aires")
--- boton2.py
+++ boton2.py
@@ -3,21 +3,32 @@ import time
 import tkinter
 
 class AppClima():
-    temp_datosHoy = "12°"
-    lugar_Hoy = "Valentin Alsina"
-    sensacion_Hoy = "12°/9° Sensacion termica 9°"
-
+    temp_datosHoy = "0"
+    lugar_Hoy = "Buenos Aires"
+    sensacion_Hoy = "0"
 
 
 
     def __init__(self):
+        self.clima = Clima()
+        self.clima.obtener_clima(self.lugar_Hoy)
+        self.temp_datosHoy = self.clima.tempHoy
+        self.sensacion_Hoy = (self.clima.tempMinHoy,self.clima.tempMaxHoy,"Sensacion termica de",self.clima.sensacionTermica)
         self.ventana = tkinter.Tk()
         self.ventana.title("Clima")
         self.ventana.resizable(height=0,width=0)
         self.ventana.geometry("400x600")
 
-        self.f1 = tkinter.Label(self.ventana, text = "Ingrese una ciudad")
+        #Inicio de la aplicacion
+
+        self.f1 = tkinter.Entry(self.ventana)
         self.f1.grid()
+
+        self.bt1 = tkinter.Button(self.ventana, text = "Obtener datos", command = self.obtenerDatos)
+        self.bt1.grid()
+        self.bt1.grid(column=0, row=1,padx=5,pady=5)
+
+        #Datos Hoy
 
         self.datosHoy = tkinter.Frame(self.ventana, bg="Blue", width=400, height=200)
 
@@ -31,19 +42,24 @@ class AppClima():
         self.fila2hoy.grid(column=0, row=2,padx=5,pady=5)
         self.fila3hoy.grid(column=0, row=3,padx=5,pady=5)
 
-        self.datosHoy.grid(column=0, row=1,padx=5,pady=5)
+        self.datosHoy.grid(column=0, row=2,padx=5,pady=5)
         
         self.fila1hoy.grid(column=0, row=0)
         self.fila2hoy.grid(column=0, row=1)
+        self.fila3hoy.grid(column=0, row=2)
 
         self.temperaturaHoy = tkinter.Frame(self.ventana,bg="Blue", width=400, height=200)
         self.temperaturaSemana = tkinter.Frame(self.ventana,bg="Blue", width=400, height=200)
 
-        self.temperaturaHoy.grid(column=0, row=2,padx=5,pady=5)
-        self.temperaturaSemana.grid(column=0, row=3,padx=5,pady=5)
+        self.temperaturaHoy.grid(column=0, row=3,padx=5,pady=5)
+        self.temperaturaSemana.grid(column=0, row=4,padx=5,pady=5)
 
         self.clima = Clima()
         self.ventana.mainloop()
+
+    def obtenerDatos(self):
+        self.clima.obtener_clima(self.f1.get())
+        self.f1.delete(first=0,last=tkinter.END)
 
 
 a = AppClima()
